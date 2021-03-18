051f71443d0a5e85f13a76172235de31e0e42cbe
Azadeh <80700763+AzadehGhahremani@users.noreply.github.com>
2021-03-18 17:31:29 +0100

    Blink

--- main.c
+++ main.c
@@ -1,21 +1,35 @@
 /*
- * Melody.c
+ * Blink.c
  *
- * 
+ * Created: 12/02/2021 3:20:09 AM
  * Author : Azadeh
  */ 
 
 #include <avr/io.h>
-#include "sound.h"
+#include <avr/sfr_defs.h>
+
+#define F_CPU 1000000UL
+#include <util/delay.h>
+
+#define BV(bit)			(1<<(bit))
+#define sbi(reg,bit)	reg |= (BV(bit))
+#define cbi(reg,bit)	reg &= ~(BV(bit))
 
 
 int main(void)
 {
     /* Replace with your application code */
+	sbi(DDRB,6);//out LED
+	cbi(DDRB,7);//input Key
+	sbi(PORTB,7);//pullup
     while (1) 
     {
-		beep(200,80);
-		beep(100,160);
+		if(bit_is_clear(PINB,7)){
+			sbi(PORTB,6);//LED on
+			_delay_ms(250);
+			cbi(PORTB,6);//LED off
+			_delay_ms(250);
+		}
     }
 }
 
